37502db50568937466c026ef6e48f2e409d4ecd4
xtayfjtn <xtayfjtn@163.com>
2017-03-03 17:11:04 +0800

    add the limit to the index

--- index/Home/Controller/IndexController.class.php
+++ index/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-      $articles = M('article')->order('article_info desc')->select();
+      $articles = M('article')->order('article_info desc')->limit(4)->select();
       // var_dump($articles);
       $this->assign('articles', $articles);
       $this->display();
